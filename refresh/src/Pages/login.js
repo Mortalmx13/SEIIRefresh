d2dd5177c58bd508400f8a902a3a1527eb0457fd
Charles Young <ctyoungiv@gmail.com>
2024-02-23 17:19:33 -0500

    Upload latest code

--- refresh/src/Pages/login.js
+++
@@ -1,9 +0,0 @@
-import FormLogin from "../Components/formlogin"
-
-export default function Login(){
-    return(
-        <div>
-            <FormLogin />
-        </div>
-    )
-}
--- refresh/src/Pages/register.js
+++
@@ -1,9 +0,0 @@
-import FormSignup from "../Components/formsignup"
-
-export default function Register(){
-    return(
-        <div>
-            <FormSignup />
-        </div>
-    )
-}
